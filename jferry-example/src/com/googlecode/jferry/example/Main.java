797585484bdeb5857b51e16d46aad1bb28b4e27e
seidel.david <seidel.david@960e0ad2-bb02-11dd-b040-2d45f096adfe>
2008-11-26 22:04:31 +0000

    initial import of example project

---
+++ jferry-example/src/com/googlecode/jferry/example/Main.java
@@ -0,0 +1,24 @@
+package com.googlecode.jferry.example;
+
+import com.googlecode.jferry.JaxRsClientProxyFactory;
+import com.googlecode.jferry.testserver.IService;
+import com.googlecode.jferry.testserver.Person;
+import com.googlecode.jferry.testserver.ServerLauncher;
+
+/**
+ * This example shows, how to use jferry. The client requires a running testserver.
+ * 
+ * @author dseidel
+ *
+ */
+public class Main {
+	public static final void main(String... args) {
+		// create a service proxy
+		IService personService = JaxRsClientProxyFactory.createProxy(IService.class, ServerLauncher.SERVER_BASE_URI);
+		
+		Person person = new Person();
+		person.setFirstName("John");
+		person.setLastName("Doe");
+		personService.addPerson(person);
+	}
+}
